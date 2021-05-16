bdaaddff068a352bc448df1a5b2bf835fa3d21a1
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-05-16 09:45:40 +0200

    Problem 2 Task 1 Finishing the metro tracking

--- P2_Task_1.pl
+++ P2_Task_1.pl
@@ -1,2 +1,14 @@
 :- use_module(metroStations).
 
+% path between 2 metroStations
+% A = starting station
+% B = destination station
+% N = number of station or any
+% R = the resulting path
+
+% fact starting station equal destination station with cut
+path(A, A, []):- !.
+
+path(A, B, [[A,X]|R]):-
+connected(A, X),
+path(X, B, R).
