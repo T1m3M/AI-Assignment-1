4277c40cc63c4def90d2142e693e12a612bb8426
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-05-17 04:52:34 +0200

    Problem 2 Task 1 adding the number of stations logic or any

--- P2_Task_1.pl
+++ P2_Task_1.pl
@@ -6,9 +6,25 @@
 % N = number of station or any
 % R = the resulting path
 
-% fact starting station equal destination station with cut
-path(A, A, []):- !.
+% interface to call find_path & check the N parameter
+path(A, B, N, R):-
+find_path(A, B, R),
+(number(N)
+	-> count(R, NR), NR = N
+	;  !
+).
 
-path(A, B, [[A,X]|R]):-
+% fact starting station equal destination station with cut
+find_path(A, A, []):- !.
+
+find_path(A, B, [[A,X]|Z]):-
 connected(A, X),
-path(X, B, R).
+find_path(X, B, Z).
+
+
+% count how many stations in result
+count([], 0).
+
+count([_|T], N):-
+count(T, N1),
+N is N1 + 1.
